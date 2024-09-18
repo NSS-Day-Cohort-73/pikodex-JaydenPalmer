ae26b3e9900eadb93f6f5ffbb8165aeeda8fc928
Jayden Palmer <jayalexpalmer@gmail.com>
2024-09-18 14:47:51 -0500

    looking better

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+import { grabData } from "./database.js";
+import { pikodexCards } from "./pikomon.js";
 const container = document.querySelector('#container')
+const pikodexData = grabData()
+const pikodexHTML = pikodexCards(pikodexData)
+//console.log(pikodexHTML)
+
+
+container.innerHTML = pikodexHTML
+
+
---
+++ scripts/pikomon.js
@@ -0,0 +1,20 @@
+
+// const pikodexData = grabData()
+
+export const pikodexCards = (data) => {
+    let pikodexHTML = ''
+    for (const pikodex of data) {
+        pikodexHTML += `
+        <article class="piko-card">
+                <img src="${pikodex.imageUrl}" alt="${pikodex.name} pic" class="piko-img">
+            <div class="info-text">
+                <p class="piko-name">${pikodex.name}</p>
+                <p class="piko-category">${pikodex.category}</p>
+                <p class="piko-abilities">${pikodex.abilities}</p>
+                <p class="piko-weakness">${pikodex.weakness}</p>
+            </div>
+        </article>
+        `;
+    }
+    return pikodexHTML
+}
